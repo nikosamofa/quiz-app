8698c6ba0a6830700ad362db7d6919fd67a06b54
Nikos Amofa <nikosbright@gmail.com>
2020-10-14 23:05:44 -0400

    final edits line formating

--- app.js
+++ app.js
@@ -60,126 +60,112 @@
 
 
 //Question database
-const store = {   
-  qns:[
-    {
-      question: 'Just how well do you know the Great Wonders of the world?',
-      buttonText: ['Start Quiz'],
-      state: 'start',
-      imageSrc: 'images/pyramid.jpg',
-      feedback:[],
-      correctAnswer: '',
-    
-    },
-  {
-      //1 [0]
-      question: 'In which modern-day country do we find petra?',
-      imageSrc: 'images/petra-jordan.jpg', 
-      imageAlt:'picture of Petra Jordan',
-      answers: [
-          'Egypt',
-          'Israel',
-          'Jordan'          
-      ],
-      correctAnswer:'Jordan',
-      buttonText:['Submit Answer','Next Question'],
-      state: 'question',
-      feedback: ['Awesome! ', 'The correct answer is Jordan.'],
-  },
-  {
-      //2 [1]
-      question: 'What does Machu Picchu mean in Quechua?',
-      imageSrc: 'images/machu-picchu.jpg',
-      imageAlt:'picture of machu picchu during the day',
-      answers: [
-          'Old peak',
-          'Edge of the mountain',
-          'Rocky peak'
-      ],
-      correctAnswer:'Old peak',
-      buttonText:['Submit Answer','Next Question'],
-      state: 'question',
-      feedback: ['Awesome! ','The correct answer is the Old Peak.'],
-  },
-  {
-      //3 [2]
-      question: 'The Chichen Itza was built by whom?',
-      imageSrc: 'images/chichen-itza.jpg',
-      imageAlt:'picture of the Chichen Itza on sun set',
-      answers: [
-          'Mayans',
-          'Egyptians',
-          'Incas'
-      ],
-      correctAnswer:'Mayans',
-       buttonText:['Submit Answer','Next Question'],
-       state: 'question',
-       feedback: ['Awesome! ', 'The correct answer is the Mayans.'],    
-  },
-  {
-      //4 [3]
-      question: 'In ancient times, what was the name of the colosseum?',
-      imageSrc: 'images/colosseum-rome.jpg',
-      imageAlt:'picture of the colosseum in the modern day setting',
-      answers: [
-          'Flavian Amphitheater',
-          'Maximus',
-          'Golden Circus'
-      ],
-      correctAnswer:'Flavian Amphitheater',
-      buttonText:['Submit Answer','Next Question'],
-      state: 'question',
-      feedback: ['Awesome! ', 'The correct answer is Flavian Amphitheater.'],
-  },
-  {
-      //5 [4]
-      question: 'In which city in India is the Taj Mahal Located?',
-      imageSrc: 'images/taj-mahal.jpg',
-      imageAlt:'picture of the Taj Mahal on sunset',
-      answers: [
-         'Agra',
-         'Delhi',
-         'Mumbai'
-      ],
-      correctAnswer:
-          'Agra',
-      buttonText:['Submit Answer','Next Question'],
-      state: 'question',
-      feedback: ['Awesome! ','The correct answer is Agra.'],
-    },  
-    
-    {
-      message: 'Finished Quiz',
-      imageSrc: 'images/5starts.png',
-      buttonText: ['Restart Quiz'],
-      answer: '',
-      feedback: [],
-      state: 'last',
-      
-    },
-  ],
-  
-  hasAnswered: false,
-  quizStart: false,
-  questionNumber: 0,
-  numRight: 0,
-  
+const store = {
+    qns: [
+        {
+            question: 'Just how well do you know the Great Wonders of the world?',
+            buttonText: ['Start Quiz'],
+            state: 'start',
+            imageSrc: 'images/pyramid.jpg',
+            feedback: [],
+            correctAnswer: '',
+        },
+        {
+            //1 [0]
+            question: 'In which modern-day country do we find petra?',
+            imageSrc: 'images/petra-jordan.jpg',
+            imageAlt: 'picture of Petra Jordan',
+            answers: [
+                'Egypt',
+                'Israel',
+                'Jordan'
+            ],
+            correctAnswer: 'Jordan',
+            buttonText: ['Submit Answer', 'Next Question'],
+            state: 'question',
+            feedback: ['Awesome! ', 'The correct answer is Jordan.'],
+        },
+        {
+            //2 [1]
+            question: 'What does Machu Picchu mean in Quechua?',
+            imageSrc: 'images/machu-picchu.jpg',
+            imageAlt: 'picture of machu picchu during the day',
+            answers: [
+                'Old peak',
+                'Edge of the mountain',
+                'Rocky peak'
+            ],
+            correctAnswer: 'Old peak',
+            buttonText: ['Submit Answer', 'Next Question'],
+            state: 'question',
+            feedback: ['Awesome! ', 'The correct answer is the Old Peak.'],
+        },
+        {
+            //3 [2]
+            question: 'The Chichen Itza was built by whom?',
+            imageSrc: 'images/chichen-itza.jpg',
+            imageAlt: 'picture of the Chichen Itza on sun set',
+            answers: [
+                'Mayans',
+                'Egyptians',
+                'Incas'
+            ],
+            correctAnswer: 'Mayans',
+            buttonText: ['Submit Answer', 'Next Question'],
+            state: 'question',
+            feedback: ['Awesome! ', 'The correct answer is the Mayans.'],
+        },
+        {
+            //4 [3]
+            question: 'In ancient times, what was the name of the colosseum?',
+            imageSrc: 'images/colosseum-rome.jpg',
+            imageAlt: 'picture of the colosseum in the modern day setting',
+            answers: [
+                'Flavian Amphitheater',
+                'Maximus',
+                'Golden Circus'
+            ],
+            correctAnswer: 'Flavian Amphitheater',
+            buttonText: ['Submit Answer', 'Next Question'],
+            state: 'question',
+            feedback: ['Awesome! ', 'The correct answer is Flavian Amphitheater.'],
+        },
+        {
+            //5 [4]
+            question: 'In which city in India is the Taj Mahal Located?',
+            imageSrc: 'images/taj-mahal.jpg',
+            imageAlt: 'picture of the Taj Mahal on sunset',
+            answers: [
+                'Agra',
+                'Delhi',
+                'Mumbai'
+            ],
+            correctAnswer:
+                'Agra',
+            buttonText: ['Submit Answer', 'Next Question'],
+            state: 'question',
+            feedback: ['Awesome! ', 'The correct answer is Agra.'],
+        },
+        {
+            message: 'Finished Quiz',
+            imageSrc: 'images/5starts.png',
+            buttonText: ['Restart Quiz'],
+            answer: '',
+            feedback: [],
+            state: 'last',
+        },
+    ],
+    hasAnswered: false,
+    quizStart: false,
+    questionNumber: 0,
+    numRight: 0,
 }
 
 
 //The template generating functions
-
-//Tracking displays 
-let pointsScored= 0;
-let counter = 1; 
-
-
-
-
 //this is the start page template
-
-function startPageTemplate(selection){
-  return `
+function startPageTemplate(selection) {
+    return `
   <div class="wrapper">
   <form id="js-form">
   <div>
@@ -197,14 +183,13 @@ function startPageTemplate(selection){
   </div>
   <p class='hide'></p>
   </form>
-  </div>
-  `
-
+  </div>`
 };
-//this is the question page template
 
-function questionTemplate(selection){  
-  return `
+
+//this is the question page template
+function questionTemplate(selection) {
+    return `
   <form class= "js-submit-feedback-form">
   <div class="wrapper">
   <div>
@@ -232,17 +217,17 @@ function questionTemplate(selection){
     <p class="hide"></p>
     <div class="item-1">
     <button class="submit feedback" type="submit">${selection.buttonText[0]}</button>
-    </div>
- 
+    </div> 
   </div>   
   </div>
   </div>
   </div>  
-</form>` 
+</form>`
 }
 
-function lastPageTemplate(selection){
-  return `
+
+function lastPageTemplate(selection) {
+    return `
   <div class="group">
   <div class="item-1">
   <form id="js-last-page-form">
@@ -255,199 +240,186 @@ function lastPageTemplate(selection){
   <button class="submit restart" type="submit">${selection.buttonText[0]}</button>  
 </form>
 </div>
-</div>
-`;
+</div>`;
 }
-//creating page slides 
 
+
+//creating page slides 
 function createTemplate(selection) {
-  switch (selection.state) {
-  case 'start':
-    return startPageTemplate(selection);
-  case 'question':
-    return questionTemplate(selection);
-  case 'last':
-    return lastPageTemplate(selection);
-  }
-} 
+    switch (selection.state) {
+        case 'start':
+            return startPageTemplate(selection);
+        case 'question':
+            return questionTemplate(selection);
+        case 'last':
+            return lastPageTemplate(selection);
+    }
+}
+
 
 //Switch through questions 
-function getIndex(){
-  let index = store.questionNumber;
-  return index;
+function getIndex() {
+    let index = store.questionNumber;
+    return index;
+}
 
-}
+
 //changing the indext
-function updateIndex(){
-  if (getIndex() < store.qns.length - 1 ){
-    store.questionNumber++;    
-  }
-  else { store.questionNumber= 1;
-  }
+function updateIndex() {
+    if (getIndex() < store.qns.length - 1) {
+        store.questionNumber++;
+    }
+    else {
+        store.questionNumber = 1;
+    }
 }
+
 
 //tracking scores 
-
-function scoreTrack(){
-  
- store.numRight++;
-
+function scoreTrack() {
+    store.numRight++;
 };
 
-//refreash score 
-function refreashSocre(){
-  store.numRight= 0;
 
+//refreash score 
+function refreashSocre() {
+    store.numRight = 0;
 };
 
 //refreshing quiz back to start
-function refreashQuiz(){
-  store.questionNumber = 1;
+function refreashQuiz() {
+    store.questionNumber = 1;
 };
+
 
 //checking for answers 
-function checkAnswer(input,entry){  
-if (input === entry.correctAnswer){ 
-   return true;
-}
-else{
-  return false;
-}
+function checkAnswer(input, entry) {
+    if (input === entry.correctAnswer) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
-//The rendering function that renders HTML and JS onto the DOM
 
-function render(){
-  const view = store.qns[getIndex()];
-  const pageView = createTemplate(view);
-  
-  $('.main').html(pageView);
+//The rendering function that renders HTML and JS onto the DOM
+function render() {
+    const view = store.qns[getIndex()];
+    const pageView = createTemplate(view);
+    $('.main').html(pageView);
 }
 
 
 //Event handler functions 
 //start the quiz
-
-function startTheQuiz(){
-  $('#js-form').on('submit', '.start', event=> {
-    event.preventDefault();
-    store.startTheQuiz = true;
-    nextquestion();
-    render();
-  })
-
+function startTheQuiz() {
+    $('#js-form').on('submit', '.start', event => {
+        event.preventDefault();
+        store.startTheQuiz = true;
+        nextQuestion();
+        render();
+    })
 }
 
-// //feedback for question 
-function PositiveFeedback(qns){  
-  $('.hide').text(qns.feedback[0] + 'you answered Question: '+ `${store.questionNumber}` + ' correct!');  
+
+//feedback for question 
+function positiveFeedback(qns) {
+    $('.hide').text(qns.feedback[0] + 'you answered Question: ' + `${store.questionNumber}` + ' correct!');
 }
 
-function negativeFeedback(qns){
-  $('.hide').text(qns.feedback[1] + ' But keep going!');
+
+function negativeFeedback(qns) {
+    $('.hide').text(qns.feedback[1] + ' But keep going!');
 };
 
-function givingFeedback(input,qns){
-  
-  if(checkAnswer(input,qns) === true){    
-  scoreTrack()  
-  return PositiveFeedback(qns);  
-}
-else{ 
 
-  return negativeFeedback(qns);
+
+function givingFeedback(input, qns) {
+    if (checkAnswer(input, qns) === true) {
+        scoreTrack()
+        return positiveFeedback(qns);
+    }
+    else {
+        return negativeFeedback(qns);
+    }
 }
 
-}
 
 //check for when the question has been answered 
-function UserAnswered(){
-  store.hasAnswered = !store.hasAnswered;
+function userAnswered() {
+    store.hasAnswered = !store.hasAnswered;
 }
 
-function checkwhenanswered(input,qns){
- 
-  if (!store.hasAnswered){
-    givingFeedback(input,qns)
-    
-  }
-  else{ 
-    UserAnswered();
-    render();
-  }
+
+function checkWhenAnswered(input, qns) {
+    if (!store.hasAnswered) {
+        givingFeedback(input, qns)
+    }
+    else {
+        userAnswered();
+        render();
+    }
 }
-//suggested 
-// function checkwhenanswered(qns,input){
-//   givingFeedback(qns,input)
-//   render();
 
-// }
-function changingSubmitClass(qns){
-  
-  $('.feedback').addClass('next');
-  $('.feedback').text(qns.buttonText[1]);
-  $('.feedback').removeClass('feedback');
 
+function changingSubmitClass(qns) {
+    $('.feedback').addClass('next');
+    $('.feedback').text(qns.buttonText[1]);
+    $('.feedback').removeClass('feedback');
 };
 
+
 //event hanlers 
-
-function getfeedback(){
-  $('main').on('submit','.js-submit-feedback-form', event=>{
-    event.preventDefault();   
-    let qns = store.qns[getIndex()]; 
-
-    let selection = $('input[name="answers"]:checked').val();
-    checkwhenanswered(selection,qns);
-    changingSubmitClass(qns);
-
-  }
-  )
+function getFeedback() {
+    $('main').on('submit', '.js-submit-feedback-form', event => {
+        event.preventDefault();
+        let qns = store.qns[getIndex()];
+        let selection = $('input[name="answers"]:checked').val();
+        checkWhenAnswered(selection, qns);
+        changingSubmitClass(qns);
+    })
 }
+
 
 //change attributes for the next button
-
-function changeNextButtonClass(qns){
-  
-  $('.next').addClass('feedback');
-  $('.next').text(qns.buttonText[0]);
-  $('.next').removeClass('next');
+function changeNextButtonClass(qns) {
+    $('.next').addClass('feedback');
+    $('.next').text(qns.buttonText[0]);
+    $('.next').removeClass('next');
 }
+
 
 //changing questions 
-function nextquestion(){
-  $('.main').on('click', '.next', event=> {
-    event.preventDefault();
-     let qns = store.qns[getIndex()];  
-     updateIndex();
-     changeNextButtonClass(qns);
-     render();
-
-
-  })
+function nextQuestion() {
+    $('.main').on('click', '.next', event => {
+        event.preventDefault();
+        let qns = store.qns[getIndex()];
+        updateIndex();
+        changeNextButtonClass(qns);
+        render();
+    })
 }
 
-function tryAgain(){
 
-  $('#js-last-page-form').on('submit','.restart', event=> {
-    event.preventDefault();
-    refreashQuiz();
-    refreashSocre();
-    render();
-
-  }
-  );
+function tryAgain() {
+    $('#js-last-page-form').on('submit', '.restart', event => {
+        event.preventDefault();
+        refreashQuiz();
+        refreashSocre();
+        render();
+    });
 }
 
 
 //call back function for all functions 
-function apps(){
-  render();
-  startTheQuiz();
-  getfeedback();
-  nextquestion();
-  tryAgain();
-
-
+function apps() {
+    render();
+    startTheQuiz();
+    getFeedback();
+    nextQuestion();
+    tryAgain();
 };
+
+
 $(apps);
